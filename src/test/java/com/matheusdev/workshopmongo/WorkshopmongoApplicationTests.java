f66d7f399d13a0c0bed2dcf6cbda2f6896505f6e
matheus <matheusvalencasilva2022@gmail.com>
2023-04-14 17:15:46 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/matheusdev/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusdev.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

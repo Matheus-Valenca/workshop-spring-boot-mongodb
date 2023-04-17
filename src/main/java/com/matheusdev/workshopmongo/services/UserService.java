eb1232bd0f864304d53648d197359146ad994c89
matheus <matheusvalencasilva2022@gmail.com>
2023-04-17 17:31:35 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/matheusdev/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.matheusdev.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusdev.workshopmongo.domain.User;
+import com.matheusdev.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}

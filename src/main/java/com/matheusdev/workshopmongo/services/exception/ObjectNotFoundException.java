61cb95082635e356d4d6bbb62f014edc000ce9b9
matheus <matheusvalencasilva2022@gmail.com>
2023-04-19 15:52:09 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/matheusdev/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.matheusdev.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}

c7c84b9150e2dc48ff757fb5bd36f5c38fd59c7f
matheus <matheusvalencasilva2022@gmail.com>
2023-04-25 17:21:24 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/matheusdev/workshopmongo/config/Instantiation.java
+++ src/main/java/com/matheusdev/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.matheusdev.workshopmongo.domain.Post;
 import com.matheusdev.workshopmongo.domain.User;
 import com.matheusdev.workshopmongo.dto.AuthorDTO;
+import com.matheusdev.workshopmongo.dto.CommentDTO;
 import com.matheusdev.workshopmongo.repository.PostRepository;
 import com.matheusdev.workshopmongo.repository.UserRepository;
 
@@ -37,10 +38,16 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userReposiroty.save(maria);
 	}
-
 }
--- src/main/java/com/matheusdev/workshopmongo/domain/Post.java
+++ src/main/java/com/matheusdev/workshopmongo/domain/Post.java
@@ -1,32 +1,31 @@
 package com.matheusdev.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Objects;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.matheusdev.workshopmongo.dto.AuthorDTO;
+import com.matheusdev.workshopmongo.dto.CommentDTO;
 
 
 @Document
 public class Post implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
-	
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
-	
-	public Post() {
-		
-	}
 
+	private List<CommentDTO> comments = new ArrayList<>();
+
+	public Post() {
+	}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
@@ -36,63 +35,52 @@ public class Post implements Serializable {
 		this.body = body;
 		this.author = author;
 	}
-
-
 	public String getId() {
 		return id;
 	}
-
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
-
 	public Date getDate() {
 		return date;
 	}
-
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
-
-
 	public String getTitle() {
 		return title;
 	}
-
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-
 	public String getBody() {
 		return body;
 	}
-
-
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
 	public AuthorDTO getAuthor() {
 		return author;
 	}
-
-
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -102,11 +90,11 @@ public class Post implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
-
-	
-	
-	
 }

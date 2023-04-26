7ec2320201fee3e12f1e09beda31e96c958fae34
matheus <matheusvalencasilva2022@gmail.com>
2023-04-26 15:00:14 -0300

    Consulta simples com @Query

--- src/main/java/com/matheusdev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/matheusdev/workshopmongo/repository/PostRepository.java
@@ -2,14 +2,17 @@ package com.matheusdev.workshopmongo.repository;
 
 import java.util.List;
 
-
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.matheusdev.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
